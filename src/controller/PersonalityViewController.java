5ce18ca21772db4aa23efcc7f753ad31582e61a2
krishnamohan <krishnamohan.py@gmail.com>
2020-06-03 10:12:59 +0530

    completed

--- src/controller/PersonalityViewController.java
+++ src/controller/PersonalityViewController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.PersonalityCalculator;
+
 
 @WebServlet(urlPatterns= {"/personality"})
 public class PersonalityViewController extends HttpServlet {
@@ -34,7 +36,8 @@ public class PersonalityViewController extends HttpServlet {
 		
 		System.out.println(options);
 				
-		/* 
+		PersonalityCalculator ref=new PersonalityCalculator(); 
+		String message=ref.findYourBrainType(options);
 
 		request.setAttribute("message", message);
 		
@@ -53,7 +56,7 @@ public class PersonalityViewController extends HttpServlet {
 				rd.forward(request, response);
 			}
 		
-		}*/
+		}
 		}
 	}
 
--- src/service/PersonalityCalculator.java
+++ src/service/PersonalityCalculator.java
@@ -1,2 +1,31 @@
 package service;
-
+public class PersonalityCalculator{
+	public String findYourBrainType(String options){
+		int [] a=  findAnswers(options);
+		int A=a[0]+a[1]+a[2]+a[4]+a[7]+a[9]+a[10]+a[11]+a[13]+a[17]+a[19];
+		int B=a[3]+a[5]+a[6]+a[8]+a[12]+a[14]+a[15]+a[16]+a[18];
+		int output=66-A+B;
+		if(output>=20 && output<=55)
+		{
+			return "leftbrained";
+		}
+		else if(output>=56 && output<=64)
+		{
+			return "No clear preference";
+		}
+		else {
+			return "rightbrained";
+		}
+	
+	}
+	
+public int [] findAnswers( String options){
+	   int[] arr1=new int [20];
+	   String[] arr2=options.split(",");
+	  for(int i=0;i<arr2.length;i++)
+	  {
+		      arr1[i]=Integer.parseInt(arr2[i]);
+	  }
+	return arr1;
+}
+}
